638e67fea2d0cbcadca335c82f89c3d1f36203e5
Nima Sepehr <nimasepehr@Nimas-MacBook-Air.local>
2014-12-04 21:28:20 -0800

    Working app with class for adding tasks

---
+++ ToDoList/XYZToDoItem.swift
@@ -0,0 +1,16 @@
+//
+//  XYZToDoItem.swift
+//  ToDoList
+//
+//  Created by Nima Sepehr on 11/30/14.
+//  Copyright (c) 2014 Nima Sepehr. All rights reserved.
+//
+
+import UIKit
+
+class XYZToDoItem: NSObject {
+    var itemName: String?
+    var completed: Bool?
+    let creationDate: NSDate?
+    
+}
---
+++ ToDoList/XYZToDoListTableViewController.swift
@@ -0,0 +1,117 @@
+//
+//  XYZToDoListTableViewController.swift
+//  ToDoList
+//
+//  Created by Nima Sepehr on 11/30/14.
+//  Copyright (c) 2014 Nima Sepehr. All rights reserved.
+//
+
+import UIKit
+
+class XYZToDoListTableViewController: UITableViewController {
+
+    var toDoItem: [XYZToDoItem] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.loadInitialData()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    func loadInitialData() {
+        // Function load some data into array toDoItem
+        var item1 = XYZToDoItem()
+        item1.itemName = "Buy milk"
+        self.toDoItem.append(item1)
+        var item2 = XYZToDoItem()
+        item2.itemName = "Buy eggs"
+        self.toDoItem.append(item2)
+        var item3 = XYZToDoItem()
+        item3.itemName = "Learn iOS"
+        self.toDoItem.append(item3)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // Return the number of sections.
+        return 1
+    }
+
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return self.toDoItem.count
+    }
+
+    @IBAction func unwindToList(segue: UIStoryboardSegue) {
+        // Left empty for now
+        //println("Im unwinding")
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ListPrototypeCell", forIndexPath: indexPath) as UITableViewCell
+
+        // Configure the cell...
+        var toDoItem = self.toDoItem[indexPath.row]
+        cell.textLabel.text = toDoItem.itemName
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
